2e8dd58297ef17fc6a151904fdf3d947f352998a
iElenV <energyzer_85@mail.ru>
2023-12-21 21:39:11 +0300

    Initial Commit

--- src/TestTool.java
+++ src/TestTool.java
@@ -1,10 +1,30 @@
 public class TestTool {
 
+
+
+    public  <T,K> boolean isEquals (T t, K k) {
+        System.out.println("Check t");
+        if (isNull(t)) {
+            return false;
+        }
+        System.out.println("Check k");
+        if (isNull(k)) {
+            return false;
+        }
+        return t.equals(k);
+    }
     public  <T> boolean isNull (T t) {
         return t==null;
     }
 
     public  <T> boolean isNotNull (T t) {
         return t!=null;
+    }
+    public  void PrintResult (boolean result) {
+        if (result) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED");
+        }
     }
 }
