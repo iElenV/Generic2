e22cedf267d95caf587760f1addb3bc7cd0202ec
iElenV <energyzer_85@mail.ru>
2023-12-21 21:44:53 +0300

    Initial Commit

--- src/CollectionTest.java
+++ src/CollectionTest.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class CollectionTest extends  TestTool{
+
+    public void testAddElement() {
+        ArrayList <Object> list = new ArrayList<>();
+        Integer first = 1;
+        Integer second = 2;
+        list.add(first);
+        list.add(second);
+        list.add(1, 67);
+
+        this.PrintResult(isEquals(first, list.get(0)));
+        this.PrintResult(isEquals(second, list.get(3)));
+    }
 }
