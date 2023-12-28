6b923a0c7263fa7d558152d433d7c8b1a6a5265a
iElenV <energyzer_85@mail.ru>
2023-12-28 21:30:26 +0300

    Commit 1

--- src/FileLogger.java
+++ src/FileLogger.java
@@ -1,17 +1,22 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FileLogger {
-    private final String PATH = "C:\\Users\\student\\Desktop\\Vavilova\\iElenV\\logs.txt";
+    private final String PATH = "C:\\Users\\student\\Desktop\\Vavilova\\iElenV\\logs_{date}.txt";
+
 
     public void Log(String msg) {
         try (FileWriter writer = new FileWriter(PATH,true)) {
-            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/d HH:mm:ss");
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/d");
             Date date = new Date();
             var currentDate = dateFormat.format(date);
             String message = String.format("[%s] %s", currentDate, msg);
+           // String filename = String.format("%s_{%s}.txt", "logs",currentDate);
+          //  String PATH1 = PATH + filename;
+          //  File file = new File(PATH1);
             writer.write(message);
             writer.append('\n');
             writer.flush();
